ca31e1742101a0495d29f6bf4f07e94770f21dc1
Vitaliy Potapov <noginsk@rambler.ru>
2021-11-24 21:20:44 +0300

    add deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve'
 const targetUrl = 'http://192.168.1.64:3000/index.html?id=1637762220-b8a7b7fd62&p=alice';
 
 export default defineConfig({
+  base: '/qrlink/',
   build: {
     outDir: `dist`,
   },
